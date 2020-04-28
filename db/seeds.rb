f7e78e99158623178cc0c5ac6a9e6008f20b7244
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-04-28 22:38:12 +0600

    Generated Model, Controller and Portfolio resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |item|
+  Portfolio.create!(
+    title: "Title number #{item}"
+    subtitle: "SubTitle number #{item}"
+    body: "Body number #{item}"
+    main_image: "https://via.placeholder.com/600x400"
+    thumb_image: "https://via.placeholder.com/300x150"
+  )
+end
