e7c996b5badb8b53bee62b9f2c63e67d4e1c2302
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-04-28 23:09:58 +0600

    Generated Sample Data for Blogs, Skills and Portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+5.times do |item|
+  Blog.create!(
+    title: "Blog Title#{item}",
+    body: "Blog Body #{item}"
+  )
+end
+
+puts "Sample data for Blogs was created successfully!"
+
+3.times do |item|
+  Skill.create!(
+    title: "Skill Number #{item}",
+    percent_utilized: 99
+  )  
+end
+
+puts "Sample data for Skills was created successfully!"
+
 10.times do |item|
   Portfolio.create!(
-    title: "Title number #{item}"
-    subtitle: "SubTitle number #{item}"
-    body: "Body number #{item}"
-    main_image: "https://via.placeholder.com/600x400"
+    title: "Title number #{item}",
+    subtitle: "SubTitle number #{item}",
+    body: "Body number #{item}",
+    main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/300x150"
   )
 end
+
+puts "Sample data for Portfolio was created successfully!"
